21456e531473390ebb99a39dd841a0619cfd7cf1
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2022-05-30 22:38:38 +0700

    Finish generate method

---
+++ internal/generator.go
@@ -0,0 +1,45 @@
+package internal
+
+import (
+	"strings"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+type generator struct {
+	*protogen.GeneratedFile
+	*protogen.File
+}
+
+func normalizeFullname(fn protoreflect.FullName) string {
+	return strings.ReplaceAll(string(fn), ".", "_")
+}
+
+func (g *generator) Generate() {
+	g.P("package ", g.GoPackageName)
+	g.P("const (")
+	for _, s := range g.Services {
+		for _, m := range s.Methods {
+			g.P("\tMethod_", normalizeFullname(s.Desc.FullName()), "__", m.Desc.Name(), ` = "`, s.Desc.FullName(), "/", m.Desc.Name(), `"`)
+		}
+	}
+	g.P(")")
+}
+
+func Run(opt protogen.Options) {
+	opt.Run(func(p *protogen.Plugin) error {
+		p.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		for _, f := range p.Files {
+			if !f.Generate {
+				continue
+			}
+			filename := f.GeneratedFilenamePrefix + "_methods.pb.go"
+			file := p.NewGeneratedFile(filename, f.GoImportPath)
+			g := generator{file, f}
+			g.Generate()
+		}
+		return nil
+	})
+}
---
+++ internal/generator_test.go
@@ -0,0 +1,76 @@
+package internal
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+var (
+	fds   *descriptorpb.FileDescriptorSet = &descriptorpb.FileDescriptorSet{}
+	files *protoregistry.Files            = &protoregistry.Files{}
+	p     *protogen.Plugin
+)
+
+func TestMain(m *testing.M) {
+	os.RemoveAll("test/test.pb.descriptor")
+	cmd := exec.Command("protoc", "-o", "test/test.pb.descriptor", "--include_imports", "-I", "test", "test.proto")
+	err := cmd.Start()
+	if err != nil {
+		panic(fmt.Errorf("failed to generate protobuf descriptor: %s, %q", err.Error(), strings.Join(cmd.Args, " ")))
+	}
+	err = cmd.Wait()
+	if err != nil {
+		panic(fmt.Errorf("failed to generate protobuf descriptor: %s, %q", err.Error(), strings.Join(cmd.Args, " ")))
+	}
+	gengocmd := exec.Command("protoc", "--go_out=:test", "--go_opt=paths=source_relative", "-I", "test", "test.proto")
+	err = gengocmd.Start()
+	if err != nil {
+		panic(fmt.Errorf("failed to generate protobuf descriptor: %s, %q", err.Error(), strings.Join(gengocmd.Args, " ")))
+	}
+	err = gengocmd.Wait()
+	if err != nil {
+		panic(fmt.Errorf("failed to generate protobuf descriptor: %s, %q", err.Error(), strings.Join(gengocmd.Args, " ")))
+	}
+	b, err := ioutil.ReadFile("./test/test.pb.descriptor")
+	if err != nil {
+		panic(fmt.Errorf("failed to parse proto descriptor: %w", err))
+	}
+	err = proto.Unmarshal(b, fds)
+	if err != nil {
+		panic(fmt.Errorf("failed to unmarshall proto descriptor: %w", err))
+	}
+	files, err = protodesc.NewFiles(fds)
+	if err != nil {
+		panic(fmt.Errorf("failed to read FileDescriptorSet: %w", err))
+	}
+	p, err = protogen.Options{}.New(&pluginpb.CodeGeneratorRequest{
+		ProtoFile: fds.File,
+	})
+	if err != nil {
+		panic(fmt.Errorf("failed to generate plugin: %w", err))
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestPlugin(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		importPath := p.FilesByPath["test.proto"].GoImportPath
+		gen := p.NewGeneratedFile("test.pb.fullmethods.go", importPath)
+		g := &generator{gen, p.FilesByPath["test.proto"]}
+		g.Generate()
+		raw, _ := g.Content()
+		fmt.Print(string(raw))
+	})
+}
